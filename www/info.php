7d2853245c051bf61266c9a21cfb3fc5cfad1e46
qiaoyuok <1589946526@qq.com>
2022-08-01 22:33:44 +0800

    修改

--- www/info.php
+++ www/info.php
@@ -1,14 +1,14 @@
 <?php
 
 phpinfo();
-// $redis = new \Redis();
-// $res = $redis->connect("redis",6379);
-//
-// if ($res){
-//     echo "连接Redis数据库成功".PHP_EOL;
-// }else{
-//     echo "连接Redis数据库失败".PHP_EOL;
-// }
+$redis = new \Redis();
+$res = $redis->connect("redis",6379);
+
+if ($res){
+    echo "连接Redis数据库成功".PHP_EOL;
+}else{
+    echo "连接Redis数据库失败".PHP_EOL;
+}
 //
 //
 // $db = mysqli_connect("mysql","root","root","demo");
